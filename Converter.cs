b9ba4255d8c58754b4a4023ff2172eb78334864c
shuwei1207 <45118501+shuwei1207@users.noreply.github.com>
2020-02-27 21:07:25 +0800

    Add files via upload

---
+++ Converter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+    public class Converter
+    {
+
+        public double FarenheitToCelsius(double temperature)
+        {
+            return (5.0 / 9.0) * (temperature - 32);
+        }
+
+        public double CelsiusToFarenheit(double temperature)
+        {
+            return (9.0 / 5.0) * temperature + 32;
+        }
+
+        public double AbsoluteToCelsius(double temperature)
+        {
+            return temperature - 273;
+        }
+
+        public double CelsiusToAbsolute(double temperature)
+        {
+            return temperature + 273;
+        }
+
+        public double FarenheitToAbsolute(double temperature)
+        {
+            return (5.0 / 9.0) * (temperature - 32) + 273;
+        }
+
+        public double AbsoluteToFarenheit(double temperature)
+        {
+            return (9.0 / 5.0) * (temperature - 273 )+ 32 ;
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Write the Farenheit temperature:");
+            string temperature1 = Console.ReadLine();
+
+            var converter1 = new Converter();
+            double result1 = converter1.FarenheitToCelsius(Convert.ToDouble(temperature1));
+
+            Console.WriteLine(String.Format("The temperature in Celsius is {0}", result1));
+            Console.ReadLine();
+
+            var converter2 = new Converter();
+            double result2 = converter1.FarenheitToAbsolute(Convert.ToDouble(temperature1));
+
+            Console.WriteLine(String.Format("The temperature in Absolute is {0}", result2));
+            Console.ReadLine();
+
+
+            Console.WriteLine("Write the Celsius temperature:");
+            string temperature2 = Console.ReadLine();
+
+            var converter3 = new Converter();
+            double result3 = converter3.CelsiusToFarenheit(Convert.ToDouble(temperature2));
+
+            Console.WriteLine(String.Format("The temperature in Farenheit is {0}", result3));
+            Console.ReadLine();
+
+            var converter4 = new Converter();
+            double result4 = converter1.CelsiusToAbsolute(Convert.ToDouble(temperature2));
+
+            Console.WriteLine(String.Format("The temperature in Absolute is {0}", result4));
+            Console.ReadLine();
+
+
+            Console.WriteLine("Write the Absolute temperature:");
+            string temperature3 = Console.ReadLine();
+
+            var converter5 = new Converter();
+            double result5 = converter3.AbsoluteToCelsius(Convert.ToDouble(temperature3));
+
+            Console.WriteLine(String.Format("The temperature in Celsius is {0}", result5));
+            Console.ReadLine();
+
+            var converter6 = new Converter();
+            double result6 = converter1.AbsoluteToFarenheit(Convert.ToDouble(temperature3));
+
+            Console.WriteLine(String.Format("The temperature in Farenheit is {0}", result6));
+            Console.ReadLine();
+        }
+    }
+}
